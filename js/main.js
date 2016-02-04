84382f6c9346ebfb81cf0fce3de27813055c577a
Luke Berry <luke@payzip.co.uk>
2016-02-04 18:52:25 +0000

    scroll to

--- js/main.js
+++ js/main.js
@@ -39,3 +39,7 @@ $(document).ready(function(){
         }
     });
 });
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top - 85},'slow');
+}
