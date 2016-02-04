e78eb91cadb08ba2bb2ade31793d31af7ffe89dc
Luke Berry <luke@payzip.co.uk>
2016-02-04 15:10:57 +0000

    Changed hero image and logo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 $(document).ready(function(){
-  $(window).scroll(function() { // check if scroll event happened
-    if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-      $(".navbar-fixed-top").css("background-color", "#c43234").css("height", "50").css("padding-top", 0); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-    } else {
-      $(".navbar-fixed-top").css("background-color", "transparent").css("height", "auto").css("padding-top", 25); // if not, change it back to transparent
+    $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+            $(".navbar-fixed-top").css("background-color", "#c43234").css("height", "50").css("padding-top", 0); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+            $(".navbar-fixed-top").css("background-color", "transparent").css("height", "auto").css("padding-top", 25); // if not, change it back to transparent
+        }
+    });
+    if($(window).width() > 1650) {
+        $('.overlay').css('height', $(window).height());
     }
-  });
+    else if($(window).width() > 1250) {
+        $('.overlay').css('height', '800');
+    }
+    else if($(window).width() < 965) {
+        $('.overlay').css('height', '600');
+    } 
+    else if($(window).width() < 700) {
+        $('.overlay').css('height', '400');
+    }
+    $(window).resize(function() {
+        if($(window).width() > 1650) {
+            $('.overlay').css('height', $(window).height());
+        }
+        else if($(window).width() > 1250) {
+            $('.overlay').css('height', '800');
+        }
+        else if($(window).width() < 965) {
+            $('.overlay').css('height', '600');
+        } 
+        else if($(window).width() < 700) {
+            $('.overlay').css('height', '400');
+        }
+    });
 });
