c3b022751154fd15f0ce431db7eba72231d78f9a
Luke Berry <luke@payzip.co.uk>
2018-01-25 11:42:15 +0000

    SEO and Performance increases (#10)
    * Remove old CNAME
    
    * SEO / Performance Optimisations
    
    - Move external CSS into one file
    - Compress images more
    
    * Optimisation
    
    * Remove built assets from version-control

--- js/main.js
+++ js/main.js
@@ -6,39 +6,6 @@ $(document).ready(function(){
             $(".navbar-fixed-top").css("background-color", "transparent").css("height", "auto").css("padding-top", 25); // if not, change it back to transparent
         }
     });
-    if($(window).width() > 1650) {
-        $('.overlay').css('height', $(window).height());
-    }
-    else if($(window).width() > 1250) {
-        $('.overlay').css('height', '800');
-    }
-    else if($(window).width() > 700) {
-        $('.overlay').css('height', '600');
-    }
-    else if($(window).width() > 500) {
-        $('.overlay').css('height', '400');
-    }
-    else {
-        $('.overlay').css('height', '350');
-    }
-    $(window).resize(function() {
-        if($(window).width() > 1650) {
-            $('.overlay').css('height', $(window).height());
-        }
-        else if($(window).width() > 1250) {
-            $('.overlay').css('height', '800');
-        }
-        else if($(window).width() > 700) {
-            $('.overlay').css('height', '600');
-        }
-        else if($(window).width() > 500) {
-            $('.overlay').css('height', '400');
-        }
-        else {
-            $('.overlay').css('height', '350');
-        }
-    });
-
 });
 function scrollToAnchor(aid){
     var aTag = $("a[name='"+ aid +"']");
